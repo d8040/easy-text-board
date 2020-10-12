eac68c1bda0b13569ff5af875b5362a76459abaa
d8040 <d8040@nate.com>
2020-10-12 11:43:21 +0900

    연습

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -2,6 +2,29 @@ package com.sbs.example.easytextboard;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("안녕");
+		String command = "article list";
+		
+		if (command.equals("article add")) {
+			System.out.println("게시물 등록");
+		}
+		
+		if (command.equals("article list")) {
+			System.out.println("게시물 리스트");
+		}
+		//System.out.println("안녕");
+		//int x = 5; 
+		//System.out.println(x);
+		
+		//int k = 20;
+		//String command = "article add";
+		//System.out.println(command);
+		
+		//if ( 10 > 5 ) { //if소중
+		//		System.out.println("참");
+			//}
+		
+		//if ( command.equals("article list")) { //if소중
+		//	System.out.println("참");	
+		//}
 	}
 }
