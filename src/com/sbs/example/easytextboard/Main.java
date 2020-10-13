debb9f286c375052014053d6c045851ec98218b5
d8040 <d8040@nate.com>
2020-10-13 11:42:34 +0900

    연습

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -3,25 +3,43 @@ package com.sbs.example.easytextboard;
 import java.util.Scanner;
 
 public class Main {
-	private static Scanner scanner;
-
 	public static void main(String[] args) {
-		scanner = new Scanner(System.in);
-		System.out.println("명령어)");
-		String command = scanner.nextLine();
-		
-		
-		if (command.equals("article list")) {
-			System.out.println("==게시물 리스트==");
-		}
-		else if (command.equals("article add")) {
-			System.out.println("==게시물 작성==");
-		}
-		else {
-			System.out.println("==존재하지 않는 명령어==");
+		Scanner scanner = new Scanner(System.in);
+		int lastArticleId = 0;
+		while (true) {
+			
+			System.out.printf("명령어) ");
+			String command = scanner.nextLine();
+			
+			if (command.equals("add")) {
 				
+				int id = 1 + lastArticleId;
+				String title;
+				String body;
+				
+				System.out.println("게시물 추가");
+				System.out.printf("제목: ");
+				title = scanner.nextLine();
+				System.out.printf("내용: ");
+				body = scanner.nextLine();
+				
+				lastArticleId = id;
+				
+				System.out.println(id +"번 게시물이 생성되었습니다.");
+				
+			} 
+			else if (command.equals("list")) {
+				System.out.println("게시물 목록");
+			} 
+			else if (command.equals("exit")) {
+				System.out.println("프로그램 종료");
+				break;
+			} 
+			else {
+				System.out.println("명령어 오류");
 			}
+			
 		}
-
+		scanner.close();
 	}
-
+}
