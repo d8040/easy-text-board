23487aced6567f5fd6bdf2a476594aa5b061cea7
d8040 <d8040@nate.com>
2020-10-17 21:37:06 +0900

    게시판 연습 201017

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -3,8 +3,9 @@ package com.sbs.example.easytextboard;
 import java.util.Scanner;
 
 public class APP {
-	Article[] articles = new Article[11];
 	
+	Article[] articles = new Article[11];
+
 	public Article getArticle(int id) {
 		return articles[id];
 	}
@@ -13,73 +14,84 @@ public class APP {
 		for (int i = 0; i < articles.length; i++) {
 			articles[i] = new Article();
 		}
-		Scanner scan = new Scanner(System.in);
 		
+		Scanner scan = new Scanner(System.in);
 		int no = 0;
 		
 		while (true) {
 			System.out.printf("명령어) ");
 			String command = scan.nextLine();
 
-			if (command.equals("add")) {
-				System.out.println("==게시물 등록==");
-				
+			if (command.equals("exit")) {
+				System.out.println("==프로그램 종료==");
+				break;
+			}
+
+			else if (command.equals("add")) {
+				System.out.println("==게시물 추가==");
+
 				int id = no + 1;
 				
-				if (id >= articles.length) {
-					System.out.println("더이상 게시물을 저장할 수 없습니다.");
-					continue;
+				if (id > articles.length) {
+					System.out.println("게시물을 더 이상 저장할 수 없습니다.");
 				}
-								
+
 				System.out.printf("제목: ");
 				String sub = scan.nextLine();
 				System.out.printf("내용: ");
 				String con = scan.nextLine();
-				System.out.println(id + "번 게시물이 저장되었습니다. ");
+				System.out.println(id + "번 게시물이 생성되었습니다.");
+
 				
-				no = id;
 				Article articles = getArticle(id);
 				articles.sub = sub;
 				articles.con = con;
-				articles.no = no;				
+				articles.no = id;
+				
+				no = id;
 			}
 			
 			else if (command.equals("list")) {
-				System.out.println("==게시판 리스트==");
+				System.out.println("==게시물 리스트==");
 				
-				if (no == 0) {
+				if(no==0) {
 					System.out.println("저장된 게시물이 없습니다.");
 					continue;
 				}
+				
 				System.out.println("번호 / 제목");
-				for (int i = 1; i <= no; i++) {
+				for(int i = 1; i<=no; i++) {
 					Article articles = getArticle(i);
-					System.out.println(articles.no +" / "+ articles.sub);
-				}		
+					System.out.println(articles.no + " / " + articles.sub);
+				}
+			}
+			else if (command.equals("detail ")) {
+				System.out.println("게시물이 존재하지 않습니다.");
+				continue;
 			}
 			
-			else if(command.startsWith("detail ")) {
+			else if (command.startsWith("detail ")) {
 				int inputId = Integer.parseInt(command.split(" ")[1]);
-				Article selArticle = getArticle(inputId);
 				
-				if(selArticle == null || selArticle.no == 0 || inputId > articles.length) {
-					System.out.println(inputId+"번 게시물이 존재하지 않습니다.");
+				
+								
+				if (articles == null || no <= 0 || inputId > articles.length) {
+					System.out.println(inputId + "번 게시물이 존재하지 않습니다.");
 					continue;
 				}
+				
+				Article selArticle = getArticle(inputId);
 				System.out.println("==게시물 상세==");
-				System.out.println("번호: " + selArticle.no);
-				System.out.println("제목: " + selArticle.sub);
-				System.out.println("내용: " + selArticle.con);
+				System.out.println("번호: "+ selArticle.no);
+				System.out.println("제목: "+ selArticle.sub);
+				System.out.println("내용: "+ selArticle.con);
+				
 			}
-			
-			else if (command.equals("exit")) {
-				System.out.println("==프로그램 종료==");
-				break;
-			}
-			
+
 			else {
-				System.out.println("**명령어 오류");
+				System.out.println("**명령어 오류**");
 			}
+
 		}scan.close();
 	}
 }
--- src/com/sbs/example/easytextboard/Article.java
+++ src/com/sbs/example/easytextboard/Article.java
@@ -1,7 +1,6 @@
 package com.sbs.example.easytextboard;
 
 public class Article {
-	
 	public String sub;
 	public String con;
 	public int no;
