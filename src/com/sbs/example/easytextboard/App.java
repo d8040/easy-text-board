bf676d37af999cee42ff95c331ab21b137fc7aa9
d8040 <d8040@nate.com>
2020-10-18 23:11:57 +0900

    201018_practice

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -2,8 +2,8 @@ package com.sbs.example.easytextboard;
 
 import java.util.Scanner;
 
-
 public class App {
+
 	Scanner scan = new Scanner(System.in);
 	Article[] articles = new Article[11];
 
@@ -18,9 +18,8 @@ public class App {
 		for (int i = 0; i < articles.length; i++) {
 			articles[i] = new Article();
 		}
-System.out.println(articles.length);
-		while (true) {
 
+		while (true) {
 			System.out.printf("명령어) ");
 			String command = scan.nextLine();
 
@@ -33,22 +32,21 @@ System.out.println(articles.length);
 				System.out.println("==게시물 등록==");
 
 				int id = no + 1;
-				
-				if (id > articles.length-1) {
-					System.out.println("더 이상 게시물을 저장할 수 없습니다.");
+
+				if (id >= articles.length) {
+					System.out.println("더 이상 게시물을 저장 할 수 없습니다.");
 					continue;
 				}
 
-				
 				System.out.printf("제목: ");
 				String sub = scan.nextLine();
 				System.out.printf("내용: ");
 				String con = scan.nextLine();
-				System.out.println(id + "번 게시물이 저장되었습니다.");
+				System.out.println(id + "번 게시물이 등록 되었습니다.");
+
 				no = id;
 
 				Article articles = getArticle(id);
-
 				articles.sub = sub;
 				articles.con = con;
 				articles.no = no;
@@ -58,7 +56,7 @@ System.out.println(articles.length);
 				System.out.println("==게시물 리스트==");
 
 				if (no == 0) {
-					System.out.println("저장된 게시물이 없습니다.");
+					System.out.println("등록된 게시물이 없습니다.");
 					continue;
 				}
 
@@ -68,6 +66,7 @@ System.out.println(articles.length);
 					Article articles = getArticle(i);
 					System.out.println(articles.no + " / " + articles.sub);
 				}
+
 			}
 
 			else if (command.equals("detail ")) {
@@ -82,10 +81,12 @@ System.out.println(articles.length);
 					System.out.println(inputId + "번 게시물이 없습니다.");
 					continue;
 				}
+				
 				Article selArticle = getArticle(inputId);
 				System.out.println("번호: " + selArticle.no);
 				System.out.println("제목: " + selArticle.sub);
 				System.out.println("내용: " + selArticle.con);
+
 			}
 
 			else {
@@ -94,6 +95,6 @@ System.out.println(articles.length);
 
 		}
 		scan.close();
-
 	}
+
 }
