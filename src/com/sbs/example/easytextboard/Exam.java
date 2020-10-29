c4fb2ec1e8d41896c4319e554be674f945aa44a4
d8040 <d8040@nate.com>
2020-10-29 19:17:02 +0900

    로그인페이지 추가_201029

--- src/com/sbs/example/easytextboard/Exam.java
+++ src/com/sbs/example/easytextboard/Exam.java
@@ -8,6 +8,7 @@ public class Exam {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		캐릭터[] 캐릭터들 = new 캐릭터[n];
+
 		for (int i = 0; i < n; i++) {
 			String 이름 = sc.next();
 			int 태어난해 = Integer.parseInt(sc.next().replace("년", ""));
@@ -17,32 +18,17 @@ public class Exam {
 
 			if (직업.equals("의적")) {
 				a캐릭터 = new 의적();
-			} else if (직업.equals("도적")) {
+			}
+			else if (직업.equals("도적")) {
 				a캐릭터 = new 도적();
-			} else if (직업.equals("상인")) {
+			}
+			else if (직업.equals("상인")) {
 				a캐릭터 = new 상인();
 			}
 
 			a캐릭터.이름 = 이름;
 			a캐릭터.번호 = i + 1;
 			a캐릭터.태어난해 = 태어난해;
-
-			캐릭터들[i] = a캐릭터;
-		}
-		for (int i = 0; i < n; i++) {
-			String 능력치들 = sc.next();
-			String[] 능력치 = 능력치들.split(",");
-			int 힘 = Integer.parseInt(능력치[0]);
-			int 지능 = Integer.parseInt(능력치[1]);
-			int 민첩 = Integer.parseInt(능력치[2]);
-
-			캐릭터들[i].힘 = 힘;
-			캐릭터들[i].지능 = 지능;
-			캐릭터들[i].민첩 = 민첩;
-		}
-		for (int i = 0; i < n; i++) {
-			캐릭터들[i].자기소개();
-			캐릭터들[i].공격();
 		}
 	}
 }
@@ -50,24 +36,18 @@ public class Exam {
 class 의적 extends 캐릭터 {
 	의적() {
 		직업 = "의적";
-		a무기 = new 검();
 	}
 }
-
 class 도적 extends 캐릭터 {
 	도적() {
 		직업 = "도적";
-		a무기 = new 도끼();
 	}
 }
-
 class 상인 extends 캐릭터 {
 	상인() {
 		직업 = "상인";
-		a무기 = new 지팡이();
 	}
 }
-
 class 캐릭터 {
 	int 번호;
 	String 이름;
@@ -78,19 +58,16 @@ class 캐릭터 {
 	int 지능;
 	int 민첩;
 	
-	무기 a무기;
-
 	int get나이() {
 		int year = Calendar.getInstance().get(Calendar.YEAR);
 		return year - 태어난해;
 	}
-
 	void 자기소개() {
 		System.out.println("== 자기소개 시작 ==");
 		System.out.println("번호: " + 번호);
 		System.out.println("이름: " + 이름);
-		System.out.println("직업: " + 직업);
-		System.out.println("태어난해: " + 태어난해);
+		System.out.println("캐릭터 종류: " + 직업);
+		System.out.println("태어난 해: " + 태어난해);
 		System.out.println("나이: " + get나이());
 		System.out.println("힘: " + 힘);
 		System.out.println("지능: " + 지능);
@@ -98,38 +75,7 @@ class 캐릭터 {
 		System.out.println("== 자기소개 끝 ==");
 	}
 	void 공격() {
-		System.out.println("== 공격 시작 ==");
-		a무기.작동(이름, 직업, 힘, 민첩);
-		System.out.println("== 공격 끝 ==");
-	}
-
-}
-class 무기{
-	String 이름;
-	int 공격력;
-	
-	void 작동(String 이름, String 직업, int 힘, int 민첩) {
-		System.out.printf("%s인 %s가 %s로 공격을 시작합니다.\n", 직업, 이름, this.이름);
-	    System.out.println("공격력: "+공격력);
-	    System.out.println("데미지: "+ 힘*민첩*공격력);
-	}
-	
-}
-class 검 extends 무기{
-	검(){
-		이름 = "검";
-		공격력 = 10;				
-	}
-}
-class 도끼 extends 무기{
-	도끼(){
-		이름 = "도끼";
-		공격력 = 15;				
-	}
-}
-class 지팡이 extends 무기{
-	지팡이(){
-		이름 = "지팡이";
-		공격력 = 2;				
+	System.out.println("== 공격 시작 ==");
+	System.out.println("== 공격 끝 ==");
 	}
 }
--- src/com/sbs/example/easytextboard/controller/MemberController.java
+++ src/com/sbs/example/easytextboard/controller/MemberController.java
@@ -11,18 +11,27 @@ public class MemberController extends Controller {
 	List<Member> members = new ArrayList<>();
 	private int memberNo = 0;
 
-	private int memberJoin(String loginId, String loginPw, String name) {
-
+	private int join(String logId, String logPw, String name) {
 		Member member = new Member();
 
 		member.no = memberNo + 1;
-		member.loginId = loginId;
-		member.loginPw = loginPw;
+
+		member.loginId = logId;
+		member.loginPw = logPw;
 		member.name = name;
 		members.add(member);
-
 		memberNo = member.no;
+
 		return member.no;
+	}
+
+	private boolean isJoinAvailableLoginId(String loginId) {
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public void run(Scanner scan, String command) {
@@ -30,30 +39,60 @@ public class MemberController extends Controller {
 		if (command.equals("member join")) {
 			System.out.println("== 회원가입 ==");
 
+			String loginId = "";
+			String loginPw;
+			String name;
+
 			int loginIdMaxCount = 3;
 			int loginIdCount = 0;
-			boolean loginIdIsvalid = false;
+			boolean loginIdIsValid = false;
 
 			while (true) {
-				if (loginIdCount >= loginIdMaxCount) {
+				if (loginIdMaxCount <= loginIdCount) {
 					System.out.println("다음에 다시 시도해 주세요.");
 					break;
-					}
-				
-					System.out.printf("로그인아이디: ");
-					String loginId = scan.nextLine().trim();
-					System.out.printf("로그인비번: ");
-					String loginPw = scan.nextLine();
-					System.out.printf("이름: ");
-					String name = scan.nextLine();
-
-					int id = memberJoin(loginId, loginPw, name);
-
-					System.out.println(id + "번 회원이 생성되었습니다.");
-
 				}
+
+				System.out.printf("로그인아이디: ");
+				loginId = scan.nextLine().trim();
+
+				if (loginId.length() == 0) {
+					loginIdCount++;
+					continue;
+				} else if (isJoinAvailableLoginId(loginId) == false) {
+					loginIdCount++;
+					System.out.println(loginId + "(은)는 이미 사용중인 아이디 입니다.");
+					continue;
+				}
+				loginIdIsValid = true;
+				break;
 			}
+
+			if (loginIdIsValid == false) {
+				return;
+			}
+			while (true) {
+				System.out.printf("로그인비번: ");
+				loginPw = scan.nextLine().trim();
+
+				if (loginPw.length() == 0) {
+					continue;
+				}
+				break;
+			}
+			while (true) {
+				System.out.printf("이름: ");
+				name = scan.nextLine().trim();
+
+				if (name.length() == 0) {
+					continue;
+				}
+				break;
+			}
+
+			int id = join(loginId, loginPw, name);
 		}
+
 	}
 
 }
